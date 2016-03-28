07c829a76662d627600614ef999e296566365c10
Valerija Spasojevic <Valerija Spasojevic>
2016-03-28 20:58:27 +0200

    New changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 function submit(emailInputFld, errorLable){
     var isValid = validateEmail($(emailInputFld).val());
     if(isValid === false){
+        $(".alert-show").show();
         $(errorLable).text('You email address is not correct');
             return;
     }
@@ -17,9 +18,31 @@ function submit(emailInputFld, errorLable){
         // see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#implementation 
         ga('send', 'event', 'Conversion', 'Subscribe', 'Subscribe for updates and beta access');
     });
+
+    
 }
         
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+function enterKeyPress1(e) {
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('submit1').click();
+        return false;
+    }
+    return true;
+}
+
+function enterKeyPress2(e) {
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('submit2').click();
+        return false;
+    }
+    return true;
 }
