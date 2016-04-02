0f42820dcbcd67410de6392acff63571ec183e28
Valerija Spasojevic <Valerija Spasojevic>
2016-04-03 01:04:34 +0200

    js fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
-function submit(emailInputFld, errorLable){
-    var isValid = validateEmail($(emailInputFld).val());
-    if(isValid === false){
-        $(".alert-show").show();
-        $(errorLable).text('You email address is not correct');
+function submit1(emailInputFld, errorLable){
+    var isValid1 = validateEmail($("#email1").val());
+    if(isValid1 === false){
+        $(".alert-show1").show();
             return;
     }
-    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$(emailInputFld).val()})
+
+    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$("#email1").val()})
     .always(function(){
         //ovde moze i neka redirekcija ili conformation popup
-        $(emailInputFld).val('');
-        $(errorLable).text('');
+        $("#email1").val('');
+        $(".alert-show1").hide();
             $("#myModal").modal();
         //location.reload();    
         
@@ -17,9 +17,29 @@ function submit(emailInputFld, errorLable){
         // ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
         // see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#implementation 
         ga('send', 'event', 'Conversion', 'Subscribe', 'Subscribe for updates and beta access');
-    });
+    });   
+}
 
-    
+function submit2(emailInputFld, errorLable){
+    var isValid2 = validateEmail($("#email2").val());
+    if(isValid2 === false){
+        $(".alert-show2").show();
+            return;
+    }
+
+    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$("#email2").val()})
+    .always(function(){
+        //ovde moze i neka redirekcija ili conformation popup
+        $("#email2").val('');
+        $(".alert-show2").hide();
+            $("#myModal").modal();
+        //location.reload();    
+        
+        // notify google analytics about event
+        // ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
+        // see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#implementation 
+        ga('send', 'event', 'Conversion', 'Subscribe', 'Subscribe for updates and beta access');
+    });   
 }
         
 function validateEmail(email) {
