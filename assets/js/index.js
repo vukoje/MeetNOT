19e8436164431fb547bbff52c3c1e7b269a24c96
vukoje <vukojekg@yahoo.com>
2016-04-03 18:06:00 +0200

    Refactoring repeated js.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,15 @@
-function submit1(emailInputFld, errorLable){
-    var isValid1 = validateEmail($("#email1").val());
-    if(isValid1 === false){
-        $(".alert-show1").show();
+function submit(emailInputFld, errorLable){
+    var isValid = validateEmail($(emailInputFld).val());
+    if(isValid === false){
+        $(errorLable).show();
             return;
     }
 
-    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$("#email1").val()})
+    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$(emailInputFld).val()})
     .always(function(){
         //ovde moze i neka redirekcija ili conformation popup
-        $("#email1").val('');
-        $(".alert-show1").hide();
-            $("#myModal").modal();
-        //location.reload();    
-        
-        // notify google analytics about event
-        // ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
-        // see https://developers.google.com/analytics/devguides/collection/analyticsjs/events#implementation 
-        ga('send', 'event', 'Conversion', 'Subscribe', 'Subscribe for updates and beta access');
-    });   
-}
-
-function submit2(emailInputFld, errorLable){
-    var isValid2 = validateEmail($("#email2").val());
-    if(isValid2 === false){
-        $(".alert-show2").show();
-            return;
-    }
-
-    $.post('https://docs.google.com/forms/d/1maFVnw1Za1ZT28KBD2Oyuooke4DbxwZw_Sg3rNGW5ZM/formResponse',{'entry.173499496':$("#email2").val()})
-    .always(function(){
-        //ovde moze i neka redirekcija ili conformation popup
-        $("#email2").val('');
-        $(".alert-show2").hide();
+        $(emailInputFld).val('');
+        $(errorLable).hide();
             $("#myModal").modal();
         //location.reload();    
         
@@ -47,21 +25,11 @@ function validateEmail(email) {
     return re.test(email);
 }
 
-function enterKeyPress1(e) {
+function enterKeyPress(e, submitId) {
     e = e || window.event;
     if (e.keyCode == 13)
     {
-        document.getElementById('submit1').click();
-        return false;
-    }
-    return true;
-}
-
-function enterKeyPress2(e) {
-    e = e || window.event;
-    if (e.keyCode == 13)
-    {
-        document.getElementById('submit2').click();
+        document.getElementById(submitId).click();
         return false;
     }
     return true;
